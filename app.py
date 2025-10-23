bb0b64fdfffc53db10ddbddfadb23f6bb04fc4d2
Chafika Boudjelida <chafika.bo@gmail.com>
2025-10-23 18:17:53 -0400

    Bind Flask to port 8080 and expose /healthcheck

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
-application = app
+application = app  # for Gunicorn
 
 @app.route('/healthcheck')
 def healthcheck():
     return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
