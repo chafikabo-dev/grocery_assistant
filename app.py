65658ecef4f3169770b49016f09d3676cb726e88
Chafika Boudjelida <chafika.bo@gmail.com>
2025-10-23 18:25:10 -0400

    Expose Flask app as 'application' for Gunicorn

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
-application = app  # for Gunicorn
+application = app  # 👈 Critical for Gunicorn
 
 @app.route('/healthcheck')
 def healthcheck():
-    return "OK", 200
+    return '', 200
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+app.run(host="0.0.0.0", port=8080)
